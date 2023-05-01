54b192632375de68429852a4f1708b16efa445f5
Bruemani <rajamanikandanreg32@gmail.com>
2023-05-01 23:10:27 +0530

    api change commit-1

--- src/main/java/com/jenkins/api/JenkinsCicdApplication.java
+++ src/main/java/com/jenkins/api/JenkinsCicdApplication.java
@@ -7,9 +7,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
+
+import java.time.format.DateTimeFormatter;
 
 import static java.time.LocalDateTime.now;
+import static java.time.format.DateTimeFormatter.ofPattern;
 import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
 import static org.springframework.http.ResponseEntity.ok;
 
@@ -20,7 +22,7 @@ public class JenkinsCicdApplication {
 
     @GetMapping(produces = APPLICATION_JSON_VALUE)
     public ResponseEntity<?> sayHello() {
-        return ok("Hello user %s is current datetime".formatted(now().toString()));
+        return ok("Hello user %s is current datetime".formatted(now().format(ofPattern("yyyy-MM-dd"))));
     }
     public static void main(String[] args) {
         SpringApplication.run(JenkinsCicdApplication.class, args);
